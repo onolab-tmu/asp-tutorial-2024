842d8f8038b5287526ddcbd8964e53e03112c24d
woodnext <shun20020611@yahoo.co.jp>
2024-06-17 15:26:59 +0900

    合成窓生成時のrangeの範囲を修正

--- skotsugi/chapter04/q05.py
+++ skotsugi/chapter04/q05.py
@@ -6,8 +6,20 @@ def synthesis_window(S: int, w: np.ndarray):
   
   s = np.array([
     np.sum([
-      w[l - m*S]**2 for m in range(-(Q-1), Q-1) if 0 < l - m*S and L > l - m*S
+      w[l - m*S]**2 
+      for m in range(-(Q-1), Q) 
+      if 0 <= l - m*S and L > l - m*S
     ]) for l in range(L) 
   ])
 
   return w / s
+
+
+if __name__ == "__main__":
+  import matplotlib.pyplot as plt
+  
+  N = 500
+  hamming = np.hamming(N)
+  ws = synthesis_window(N, hamming)
+  plt.plot(ws)
+  plt.savefig("./skotsugi/chapter04/q05.png")
